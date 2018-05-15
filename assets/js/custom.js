70cd0d6bd1f9ba9bba6bd14465263e4e82e78c3c
Manendra95 <manendrajain95@gmail.com>
2018-05-15 09:34:18 +0530

    Auto-generated commit

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,32 +1,7 @@
 /*global jQuery:false */
 
 (function ($) {
-    //Google Map
-    var get_latitude = $('#google-map').data('latitude');
-    var get_longitude = $('#google-map').data('longitude');
-
-    function initialize_google_map() {
-        var myLatlng = new google.maps.LatLng(get_latitude, get_longitude);
-        var mapOptions = {
-            zoom: 14,
-            scrollwheel: false,
-            center: myLatlng
-        };
-        var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
-        var marker = new google.maps.Marker({
-            position: myLatlng,
-            map: map
-        });
-    }
-    google.maps.event.addDomListener(window, 'load', initialize_google_map);
-
-	$(function () {
-		thisyear = new Date().getFullYear();
-		$('#currentyear').text(thisyear);
-	  });
-	  
-		  
-
+    
 	$(window).load(function(){
       $("#navigation").sticky({ topSpacing: 0 });
     });
